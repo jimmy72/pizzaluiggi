94f279730dd5f17af7d732eecb069c1602e8b403
Jimmy <Jimmy@PC-JIMMY.home>
2018-08-23 11:09:22 +0200

    first commit (index.html)

---
+++ src/main/java/be/vdab/pizzaluiggi/PizzaluiggiApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.pizzaluiggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaluiggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaluiggiApplication.class, args);
+	}
+}
