7001b462061cf8e2759456b6b52933c67b5efbaf
Jimmy <Jimmy@PC-JIMMY.home>
2018-08-23 13:33:39 +0200

    9.4 Data doorgeven van de controller naar de JSP

--- src/main/java/be/vdab/pizzaluiggi/web/IndexController.java
+++ src/main/java/be/vdab/pizzaluiggi/web/IndexController.java
@@ -5,14 +5,24 @@ import java.time.LocalTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-//hier komen enkele imports. Eclipse voegt ontbrekende imports toe
-//aan je source met de sneltoetsen shift+ctrl+o.
+//Eclipse voegt ontbrekende imports toe aan je source met de sneltoetsen shift+ctrl+o.
+
 @Controller
 @RequestMapping("/")
-class IndexController {
+class IndexController { 
+	
+	private static final String JSP_FILE = "/WEB-INF/JSP/index.jsp";
+	private static final String MESSAGE_NAME = "message";
+	
 	@GetMapping 
-	String index() { 
-		return "/WEB-INF/JSP/index.jsp";
+	ModelAndView index() { 
+		String message;
+		int hour = LocalTime.now().getHour();
+		message = (hour >= 6 && hour < 12) ? "Goede morgen" :
+			(hour >= 12 && hour < 18) ? "Goede middag" :
+			(hour >= 0 && hour < 6) ? "Goede nacht" : "Goede avond";
+		return new ModelAndView(JSP_FILE, MESSAGE_NAME, message);
 	}
 }
