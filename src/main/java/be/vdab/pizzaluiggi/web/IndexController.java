5104ed2bf49677ca2177887a938e635013a11a7d
Jimmy <Jimmy@PC-JIMMY.home>
2018-08-23 13:14:13 +0200

    9.3 Controller Bean p. 26

--- src/main/java/be/vdab/pizzaluiggi/web/IndexController.java
+++ src/main/java/be/vdab/pizzaluiggi/web/IndexController.java
@@ -2,20 +2,17 @@ package be.vdab.pizzaluiggi.web;
 
 import java.time.LocalTime;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //hier komen enkele imports. Eclipse voegt ontbrekende imports toe
 //aan je source met de sneltoetsen shift+ctrl+o.
-@RestController
+@Controller
 @RequestMapping("/")
 class IndexController {
 	@GetMapping 
 	String index() { 
-		int uur = LocalTime.now().getHour();
-		return 	(uur >= 6 && uur < 12)  ? "Goede morgen!" :
-				(uur >= 12 && uur < 18) ? "Goede middag!" : 
-				(uur >= 0 && uur < 6)   ? "Goede nacht!"  : "Goede avond!";
+		return "/WEB-INF/JSP/index.jsp";
 	}
 }
---
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" session="false" trimDirectiveWhitespaces="true" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<title>Pizza Luigi</title>
+	<link rel='icon' href='images/pizza.ico' type='image/x-icon'> 
+	<meta name='viewport' content='width=device-width,initial-scale=1'>
+	<link rel='stylesheet' href='css/pizzaluiggi.css'>
+</head>
+<body>
+	<h1>Pizza Luigi</h1>
+	<img src='images/pizza.jpg' alt='pizza' class='fullwidth'>
+	<h2>Goede dag</h2>
+</body>
+</html>
