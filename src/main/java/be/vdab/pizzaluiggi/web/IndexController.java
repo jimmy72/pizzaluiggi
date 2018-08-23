af15c620e905811dfcc06d6b7f4ba66ebe3d876a
Jimmy <Jimmy@PC-JIMMY.home>
2018-08-23 12:13:36 +0200

    Test automatically restart when editing file

--- src/main/java/be/vdab/pizzaluiggi/web/IndexController.java
+++ src/main/java/be/vdab/pizzaluiggi/web/IndexController.java
@@ -14,8 +14,8 @@ class IndexController {
 	@GetMapping 
 	String index() { 
 		int uur = LocalTime.now().getHour();
-		return 	(uur >= 6 && uur < 12)  ? "Goede morgen" :
-				(uur >= 12 && uur < 18) ? "Goede middag" : 
-				(uur >= 0 && uur < 6)   ? "Goede nacht"  : "Goede avond";
+		return 	(uur >= 6 && uur < 12)  ? "Goede morgen!" :
+				(uur >= 12 && uur < 18) ? "Goede middag!" : 
+				(uur >= 0 && uur < 6)   ? "Goede nacht!"  : "Goede avond!";
 	}
 }
